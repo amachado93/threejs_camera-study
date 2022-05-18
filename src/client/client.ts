9f4d661d2e65a8447c204ff0d87e4177f84c4981
Alex Machado <amachado@sva.edu>
2022-05-17 21:42:42 -0400

    Create different camera views

--- src/client/client.ts
+++ src/client/client.ts
@@ -3,17 +3,34 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 const scene = new THREE.Scene()
 
-const camera = new THREE.PerspectiveCamera(
-    75, window.length / window.innerHeight, 0.1, 1000
-)
+const camera1 = new THREE.PerspectiveCamera(75, 1, 0.1, 10)
+const camera2 = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 10)
+const camera3 = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 10)
+const camera4 = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 10)
 
-camera.position.z = 2
+camera1.position.z = 2
+camera2.position.y = 1
+camera2.lookAt(new THREE.Vector3(0, 0, 0))
+camera3.position.z = 1
+camera4.position.x = 1
+camera4.lookAt(new THREE.Vector3(0, 0, 0))
 
-const renderer = new THREE.WebGLRenderer()
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
+const canvas1 = document.getElementById('c1') as HTMLCanvasElement
+const canvas2 = document.getElementById('c2') as HTMLCanvasElement
+const canvas3 = document.getElementById('c3') as HTMLCanvasElement
+const canvas4 = document.getElementById('c4') as HTMLCanvasElement
+const renderer1 = new THREE.WebGLRenderer({ canvas: canvas1 })
+renderer1.setSize(200, 200)
+const renderer2 = new THREE.WebGLRenderer({ canvas: canvas2 })
+renderer2.setSize(200, 200)
+const renderer3 = new THREE.WebGLRenderer({ canvas: canvas3 })
+renderer3.setSize(200, 200)
+const renderer4 = new THREE.WebGLRenderer({ canvas: canvas4 })
+renderer4.setSize(200, 200)
 
-new OrbitControls(camera, renderer.domElement)
+//document.body.appendChild(renderer.domElement)
+
+new OrbitControls(camera1, renderer1.domElement)
 
 const geometry = new THREE.BoxGeometry()
 const material = new THREE.MeshBasicMaterial({
@@ -24,14 +41,6 @@ const material = new THREE.MeshBasicMaterial({
 const cube = new THREE.Mesh(geometry, material)
 scene.add(cube)
 
-window.addEventListener('resize', onWindowResize, false)
-function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight
-    camera.updateProjectionMatrix()
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    render()
-}
-
 function animate() {
     requestAnimationFrame(animate)
 
@@ -41,8 +50,11 @@ function animate() {
     render()
 }
 
-function render(){
-    renderer.render(scene, camera)
+function render() {
+    renderer1.render(scene, camera1)
+    renderer2.render(scene, camera2)
+    renderer3.render(scene, camera3)
+    renderer4.render(scene, camera4)
 }
 
 animate()
